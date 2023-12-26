4d9415e688c29b37f2b72a86272ce01153d6cc85
debayan-oneLot <i.debayan@one-lot.com>
2023-12-27 04:51:50 +0530

    on going to the new page the other user goes to offline and the name disappears

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,170 +1,164 @@
 package handlers
 
 import (
-	
+	"fmt"
+	"github.com/CloudyKit/jet/v6"
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"sort"
-
-	"github.com/CloudyKit/jet/v6"
-	"github.com/gorilla/websocket"
 )
 
-var wsChan=make(chan WsPayload)
+var wsChan = make(chan WsPayload)
 
-var clients=make(map[WebSocketConnection]string)
+var clients = make(map[WebSocketConnection]string)
 
-var views=jet.NewSet(
+// views is the jet view set
+var views = jet.NewSet(
 	jet.NewOSFileSystemLoader("./html"),
 	jet.InDevelopmentMode(),
 )
 
-var upgradeConnection=websocket.Upgrader{
-	ReadBufferSize: 1024,
+// upgradeConnection is the websocket upgrader from gorilla/websockets
+var upgradeConnection = websocket.Upgrader{
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin: func(r *http.Request) bool {return true},
-
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-type WebSocketConnection struct{
+// Home renders the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type WebSocketConnection struct {
 	*websocket.Conn
 }
+
+// WsJsonResponse defines the response sent back from websocket
 type WsJsonResponse struct {
-	Action      string `json:"action"`
-	Message     string `json:"message"`
-	MessageType string `json:"message_type"`
+	Action         string   `json:"action"`
+	Message        string   `json:"message"`
+	MessageType    string   `json:"message_type"`
 	ConnectedUsers []string `json:"connected_users"`
 }
 
-type WsPayload struct{
-	Action      string `json:"action"`
-	Username      string `json:"username"`
-	Message     string `json:"message"`
-	Conn        WebSocketConnection `json:"-"`
+// WsPayload defines the websocket request from the client
+type WsPayload struct {
+	Action   string              `json:"action"`
+	Username string              `json:"username"`
+	Message  string              `json:"message"`
+	Conn     WebSocketConnection `json:"-"`
 }
 
-func Home(w http.ResponseWriter,r *http.Request) {
-
-	err:=renderPage(w,"home.jet",nil)
-	if err!=nil{
+// WsEndpoint upgrades connection to websocket
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgradeConnection.Upgrade(w, r, nil)
+	if err != nil {
 		log.Println(err)
 	}
 
+	log.Println("Client connected to endpoint")
 
-
-
-
-}
-
-func WsEndpoint(w http.ResponseWriter,r *http.Request){
-	ws,err:=upgradeConnection.Upgrade(w,r,nil)
-	if err!=nil{
-		log.Println(err)
-	}
-	log.Println("Client connected to Endpoint")
-	// defer ws.Close()
 	var response WsJsonResponse
-	response.Message =`<em><small>Connected to server </small></em>`
+	response.Message = `<em><small>Connected to server</small></em>`
 
+	conn := WebSocketConnection{Conn: ws}
+	clients[conn] = ""
 
-
-	conn:=WebSocketConnection{Conn: ws}
-	clients[conn]=""
-
-	
-	err=ws.WriteJSON(response)
-	if err!=nil{
+	err = ws.WriteJSON(response)
+	if err != nil {
 		log.Println(err)
 	}
 
 	go ListenForWs(&conn)
-
-
-
-
 }
 
-
-func ListenForWs(conn *WebSocketConnection){
-	 defer func(){
-		if r:=recover();r!=nil{
-			log.Println(r)
+func ListenForWs(conn *WebSocketConnection) {
+	defer func() {
+		if r := recover(); r != nil {
+			log.Println("Error", fmt.Sprintf("%v", r))
 		}
-	 }()
+	}()
 
+	var payload WsPayload
 
-	 var payload WsPayload
-
-	 for {
-		err:=conn.ReadJSON(&payload)
-		if err!=nil{
-
-		}else{
-			payload.Conn=*conn
-			wsChan<-payload
+	for {
+		err := conn.ReadJSON(&payload)
+		if err != nil {
+			// do nothing
+		} else {
+			payload.Conn = *conn
+			wsChan <- payload
 		}
-	 }
-}
-
-func ListenToWsChannel(){
-	var response WsJsonResponse
-	
-	for{
-		e:=<-wsChan 
-
-
-		switch e.Action{
-		case"username":
-			clients[e.Conn]=e.Username
-			users:=getUserList()
-			response.Action="List_users"
-			response.ConnectedUsers=users
-			BroadCastToAll(response)
-
-		}
-		// response.Action="Got Here"
-		// response.Message=fmt.Sprintf("Some message and action was %s", e.Action)
-		// BroadCastToAll(response)
-
 	}
 }
 
-func getUserList()[]string{
+func ListenToWsChannel() {
+	var response WsJsonResponse
+
+	for {
+		e := <-wsChan
+
+		switch e.Action {
+		case "username":
+			// get a list of all users and send it back via broadcast
+			clients[e.Conn] = e.Username
+			users := getUserList()
+			response.Action = "list_users"
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		case "left":
+			// handle the situation where a user leaves the page
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
+		}
+	}
+}
+
+func getUserList() []string {
 	var userList []string
-	for _,x:=range clients{
-		userList=append(userList,x)
+	for _, x := range clients {
+		if x != "" {
+			userList = append(userList, x)
+		}
 	}
 	sort.Strings(userList)
 	return userList
 }
 
-func BroadCastToAll(response WsJsonResponse){
-	for client:=range clients{
-		err:=client.WriteJSON(response)
-		if err!=nil{
-			log.Println("websocket error")
-			_=client.Close()
-			delete(clients,client)
+func broadcastToAll(response WsJsonResponse) {
+	for client := range clients {
+		err := client.WriteJSON(response)
+		if err != nil {
+			log.Println("websocket err")
+			_ = client.Close()
+			delete(clients, client)
 		}
 	}
 }
 
-
-func renderPage(w http.ResponseWriter,tmpl string,data jet.VarMap)error{
-	view,err:=views.GetTemplate(tmpl)
-	if err!=nil{
+// renderPage renders a jet template
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
 		log.Println(err)
 		return err
 	}
 
-	err=view.Execute(w,data,nil)
-
-	if err!=nil{
+	err = view.Execute(w, data, nil)
+	if err != nil {
 		log.Println(err)
 		return err
 	}
+
 	return nil
-
-
-
 }
